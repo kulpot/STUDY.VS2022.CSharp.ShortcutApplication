a3881280b3809a16ba4428a8d07f72d0325470cc
Kulpot <kulpotwahu@gmail.com>
2023-05-24 05:23:08 +0800

    Add project files.

---
+++ ShortcutApplication/Form1.cs
@@ -0,0 +1,28 @@
+﻿using IWshRuntimeLibrary;
+using System;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace ShortcutApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            CreateShortcutToCurrentAssembly(folder);
+        }
+        
+        private void CreateShortcutToCurrentAssembly(string saveDir)
+        {
+            //WshShellClass wshShell = new WshShellClass();
+            WshShell wshShell = new WshShell();
+            string fileName = saveDir + "\\" + ProductName + ".lnk";
+            IWshShortcut shortcut = (IWshShortcut)wshShell.CreateShortcut(fileName);
+            shortcut.TargetPath = Application.ExecutablePath;
+            shortcut.Save();
+        }
+    }
+}
